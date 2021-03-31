d6d1fc8d4f4000a4b8d94684b583c2cc2e5caf9b
Vikas Singh Chauhan <49335753+99Vikas@users.noreply.github.com>
2021-03-31 14:54:31 +0530

    Delete Spring Core Assignment directory

--- Spring Core Assignment/Question 3/BankAccountService.java
+++
@@ -1,9 +0,0 @@
-package springcore.com;
-
-public interface BankAccountService {
-	public double withdraw(int accountId, double balance);
-	public double deposit(int accountId, double balance); 
-	public double getBalance(int accountId);
-	public boolean fundTransfer(long fromAccount, long toAccount, double amount);
-		
-}
--- Spring Core Assignment/Question 7/README.txt
+++
@@ -1,1 +0,0 @@
-please add XML file under 'src'
