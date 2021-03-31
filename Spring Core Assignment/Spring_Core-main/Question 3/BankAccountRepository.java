9327b99189350741ba86931e696df1553c6a8c56
Vikas Singh Chauhan <49335753+99Vikas@users.noreply.github.com>
2021-03-31 13:35:42 +0530

    Delete Spring Core Assignment/Spring_Core-main directory

--- Spring Core Assignment/Spring_Core-main/Question 3/BankAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package springcore.com;
-
-public interface BankAccountRepository {
-
-public double getBalance(int accountId);
-public double updateBalance(int accountId, double newBalance,String type);
-}
